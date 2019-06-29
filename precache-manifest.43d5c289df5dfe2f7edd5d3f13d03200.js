c4e90a7bb2cc2114f33a4664b469a73e1dc50c18
tilaks1977 <tilak.sanyal@gmail.com>
2019-06-29 16:09:09 +0530

    Updates

---
+++ precache-manifest.43d5c289df5dfe2f7edd5d3f13d03200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990cc48df2b5a1c8f2fad2760c24cd54",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "f4095ed4f12849df9de8",
+    "url": "/first-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/first-react-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f4095ed4f12849df9de8",
+    "url": "/first-react-app/static/js/main.c72cc2b2.chunk.js"
+  },
+  {
+    "revision": "cb2d0d1ef7d3e2b845c4",
+    "url": "/first-react-app/static/js/runtime~main.f5c4d8dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
